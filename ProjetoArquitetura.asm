464226a260a071d23a7df1ef2f75de6c34078cf3
jpeterutto <149285505+jpeterutto@users.noreply.github.com>
2024-10-15 02:02:08 -0300

    Temperatura da bebida aparecendo no Display de Sete Segmentos

--- ProjetoArquitetura.asm
+++ ProjetoArquitetura.asm
@@ -1,237 +1,187 @@
- RS      equ     P1.3
- EN      equ     P1.2
+RS      equ     P1.3
+EN      equ     P1.2
 
-org 0000h
-	LJMP START
+ORG 0000H
 
-org 0030h
-START:
-	acall lcd_init
-	ACALL posicionaCursor 
+start:
+    ACALL lcd_init
+    ACALL displayLCD
 
-	; Exibe "Selecione o tipo"
-	MOV A, #'S'
-	ACALL sendCharacter
-	CALL delay
+main_loop:
+    ACALL displaySegments
+    JMP main_loop
 
-	MOV A, #'e'
-	ACALL sendCharacter
-	CALL delay
+displaySegments:
+    SETB P3.4
+    SETB P3.3
+    MOV P1, #10100100B
+    CALL delay
 
-	MOV A, #'l'
-	ACALL sendCharacter
-	CALL delay
+    CLR P3.3
+    SETB P3.4
+    MOV P1, #11000000B
+    CALL delay
 
-	MOV A, #'e'
-	ACALL sendCharacter
-	CALL delay
+    RET
 
-	MOV A, #'c'
-	ACALL sendCharacter
-	CALL delay
+displayLCD:
+    MOV A, #'S'
+    ACALL sendCharacter
+    MOV A, #'e'
+    ACALL sendCharacter
+    MOV A, #'l'
+    ACALL sendCharacter
+    MOV A, #'e'
+    ACALL sendCharacter
+    MOV A, #'c'
+    ACALL sendCharacter
+    MOV A, #'i'
+    ACALL sendCharacter
+    MOV A, #'o'
+    ACALL sendCharacter
+    MOV A, #'n'
+    ACALL sendCharacter
+    MOV A, #'e'
+    ACALL sendCharacter
+    MOV A, #' '
+    ACALL sendCharacter
+    MOV A, #'o'
+    ACALL sendCharacter
+    MOV A, #' '
+    ACALL sendCharacter
+    MOV A, #'t'
+    ACALL sendCharacter
+    MOV A, #'i'
+    ACALL sendCharacter
+    MOV A, #'p'
+    ACALL sendCharacter
+    MOV A, #'o'
+    ACALL sendCharacter
 
-	MOV A, #'i'
-	ACALL sendCharacter
-	CALL delay
+    ACALL segundaLinha
 
-	MOV A, #'o'
-	ACALL sendCharacter
-	CALL delay
+    MOV A, #'d'
+    ACALL sendCharacter
+    MOV A, #'e'
+    ACALL sendCharacter
+    MOV A, #' '
+    ACALL sendCharacter
+    MOV A, #'C'
+    ACALL sendCharacter
+    MOV A, #'a'
+    ACALL sendCharacter
+    MOV A, #'f'
+    ACALL sendCharacter
+    MOV A, #'e'
+    ACALL sendCharacter
 
-	MOV A, #'n'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'e'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #' '
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'o'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #' '
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'t'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'i'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'p'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'o'
-	ACALL sendCharacter
-	CALL delay
-
-	; Muda para a segunda linha do display
-	ACALL segundaLinha
-
-	; Exibe "de Café"
-	MOV A, #'d'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'e'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #' '
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'C'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'a'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'f'
-	ACALL sendCharacter
-	CALL delay
-
-	MOV A, #'e'
-	ACALL sendCharacter
-	CALL delay
-
-	JMP $
+    RET
 
 lcd_init:
-	CLR RS
-	CLR P1.7
-	CLR P1.6
-	SETB P1.5
-	CLR P1.4
-	SETB EN
-	CLR EN
-	CALL delay
-	SETB EN
-	CLR EN
-	SETB P1.7
-	SETB EN
-	CLR EN
-	CALL delay
-	CLR P1.7
-	CLR P1.6
-	CLR P1.5
-	CLR P1.4
-	SETB EN
-	CLR EN
-	SETB P1.6
-	SETB P1.5
-	SETB EN
-	CLR EN
-	CALL delay
-	CLR P1.7
-	CLR P1.6
-	CLR P1.5
-	CLR P1.4
-	SETB EN
-	CLR EN
-	SETB P1.7
-	SETB P1.6
-	SETB P1.5
-	SETB P1.4
-	SETB EN
-	CLR EN
-	CALL delay
-	RET
+    CLR RS
+    CLR P1.7
+    CLR P1.6
+    SETB P1.5
+    CLR P1.4
+    SETB EN
+    CLR EN
+    CALL short_delay
+    SETB EN
+    CLR EN
+    SETB P1.7
+    SETB EN
+    CLR EN
+    CALL short_delay
+    CLR P1.7
+    CLR P1.6
+    CLR P1.5
+    CLR P1.4
+    SETB EN
+    CLR EN
+    SETB P1.6
+    SETB P1.5
+    SETB EN
+    CLR EN
+    CALL short_delay
+    CLR P1.7
+    CLR P1.6
+    CLR P1.5
+    CLR P1.4
+    SETB EN
+    CLR EN
+    SETB P1.7
+    SETB P1.6
+    SETB P1.5
+    SETB P1.4
+    SETB EN
+    CLR EN
+    CALL short_delay
+    RET
 
 sendCharacter:
-	SETB RS
-	MOV C, ACC.7
-	MOV P1.7, C
-	MOV C, ACC.6
-	MOV P1.6, C
-	MOV C, ACC.5
-	MOV P1.5, C
-	MOV C, ACC.4
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	MOV C, ACC.3
-	MOV P1.7, C
-	MOV C, ACC.2
-	MOV P1.6, C
-	MOV C, ACC.1
-	MOV P1.5, C
-	MOV C, ACC.0
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	CALL delay
-	RET
-
-posicionaCursor:
-	CLR RS
-	SETB P1.7
-	MOV C, ACC.6
-	MOV P1.6, C
-	MOV C, ACC.5
-	MOV P1.5, C
-	MOV C, ACC.4
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	MOV C, ACC.3
-	MOV P1.7, C
-	MOV C, ACC.2
-	MOV P1.6, C
-	MOV C, ACC.1
-	MOV P1.5, C
-	MOV C, ACC.0
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	CALL delay
-	RET
+    SETB RS
+    MOV C, ACC.7
+    MOV P1.7, C
+    MOV C, ACC.6
+    MOV P1.6, C
+    MOV C, ACC.5
+    MOV P1.5, C
+    MOV C, ACC.4
+    MOV P1.4, C
+    SETB EN
+    CLR EN
+    MOV C, ACC.3
+    MOV P1.7, C
+    MOV C, ACC.2
+    MOV P1.6, C
+    MOV C, ACC.1
+    MOV P1.5, C
+    MOV C, ACC.0
+    MOV P1.4, C
+    SETB EN
+    CLR EN
+    CALL short_delay
+    RET
 
 segundaLinha:
-	; Muda o cursor para o início da segunda linha
-	CLR RS
-	MOV A, #0C0h  ; Endereço da segunda linha no LCD
-	ACALL sendCommand
-	CALL delay
-	RET
+    CLR RS
+    MOV A, #0C0h
+    ACALL sendCommand
+    RET
 
 sendCommand:
-	CLR RS
-	MOV C, ACC.7
-	MOV P1.7, C
-	MOV C, ACC.6
-	MOV P1.6, C
-	MOV C, ACC.5
-	MOV P1.5, C
-	MOV C, ACC.4
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	MOV C, ACC.3
-	MOV P1.7, C
-	MOV C, ACC.2
-	MOV P1.6, C
-	MOV C, ACC.1
-	MOV P1.5, C
-	MOV C, ACC.0
-	MOV P1.4, C
-	SETB EN
-	CLR EN
-	CALL delay
-	RET
+    CLR RS
+    MOV C, ACC.7
+    MOV P1.7, C
+    MOV C, ACC.6
+    MOV P1.6, C
+    MOV C, ACC.5
+    MOV P1.5, C
+    MOV C, ACC.4
+    MOV P1.4, C
+    SETB EN
+    CLR EN
+    MOV C, ACC.3
+    MOV P1.7, C
+    MOV C, ACC.2
+    MOV P1.6, C
+    MOV C, ACC.1
+    MOV P1.5, C
+    MOV C, ACC.0
+    MOV P1.4, C
+    SETB EN
+    CLR EN
+    CALL short_delay
+    RET
 
 delay:
-	MOV R0, #50
-	DJNZ R0, $
-	RET
+    MOV R0, #200
+delay_loop:
+    DJNZ R0, delay_loop
+    RET
+
+short_delay:
+    MOV R0, #50
+short_delay_loop:
+    DJNZ R0, short_delay_loop
+    RET
